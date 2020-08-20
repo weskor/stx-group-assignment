1a86b354eec07a1e31c2520805e252d68240cf12
Wesley van de Korput <wesley@kersvers.team>
2020-08-20 14:24:20 +0200

    Fix build

--- vue.config.js
+++ vue.config.js
@@ -1,13 +1,14 @@
 module.exports = {
+  publicPath: '',
   configureWebpack: {
     entry: {
       app: './src/js/main.ts'
     }
   },
   chainWebpack(config) {
-    config.resolve.alias.delete("@")
+    config.resolve.alias.delete('@')
     config.resolve
-      .plugin("tsconfig-paths")
-      .use(require("tsconfig-paths-webpack-plugin"))
+      .plugin('tsconfig-paths')
+      .use(require('tsconfig-paths-webpack-plugin'))
   }
 }
